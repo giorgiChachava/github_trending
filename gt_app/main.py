c3a50e7bb8999718db1efa672890316d9e0b61c0
giorgichachava <giorgi.chachava456@gmail.com>
2026-08-02 20:34:47 +0400

    api connection

--- gt_app/main.py
+++ gt_app/main.py
@@ -8,6 +8,9 @@ from datetime import datetime, timedelta
 TODAY_OBJ = datetime.today()
 TODAY_STR = TODAY_OBJ.strftime('%Y-%m-%d')
 
+MAX_DURATION_DAYS = 365  
+MAX_LIMIT = 100          
+
 
 def show_greetings():
     typeout2("---------------------------")
@@ -24,37 +27,84 @@ def show_greetings():
     time.sleep(0.3)
     typeout2("----- Hello, I am here to help you discover trending repositories on GitHub")
     typeout2("----- You can call me GT.")
-    typeout2("----- To use me, run: github_trending --duration <day> --limit <n>" 'recommended: use <n>=10')
+    typeout2("----- To use me, run: github_trending --duration <day> --limit <n>")
     typeout2("----- Now u will have information about GitHub repos that was created from <day> to today. List will be sorted by stars.")
+    typeout2("----- OFC '--duration' and '--limit' are optional. if u dont use these commands, we make them 7 and 10 respectivly.")
     typeout2("----- Now we can explore what's hot on GitHub without opening our browser. HOOORAYYY")
 
+def fetch_data(from_data, limit):
+    query = f'created:{from_data}..{TODAY_STR}&sort=stars&order=desc'
+    url = f"https://api.github.com/search/repositories?q={query}"
+    response = requests.get(url)
+    if response.status_code==200:
+        data = response.json()
+        counter=0
+        for item in data['items']:
+            if counter!=limit:
+                counter+=1
+                typeout3(f"----- repo N{counter} name: {item['name']}")
+                typeout3(f"----- repo N{counter} stars: {item['stargazers_count']}")
+                typeout3(f"----- repo N{counter} url: {item['html_url']}")
+                typeout3("----------------------------------")
+            else:
+                return 
+                
+                
+        print(data)
+        print(from_data)
+        return
+    elif response.status_code == 403:
+        typeout3("----- Rate limit exceeded! Wait a minute before running again.")
+    elif response.status_code == 304:
+        typeout3("----- Sorry, Error N304")
+    elif response.status_code == 422:
+        typeout3("----- Sorry, Error N422")
+    elif response.status_code == 503:
+        typeout3("----- Sorry, Error N503") 
 
+def error_messege():
+    typeout("----- Nuhuh. To use me, run: github_trending --duration <day> --limit <n>")
+    typeout("----- OFC '--duration' and '--limit' are optional. if u dont use these commands, we make them 7 and 10 respectivly.")
+    return
 def main():
     args = sys.argv[1:]
     if len(args)==0:
-        show_greetings()
-        print(int(TODAY[8:]))
-    elif len(args)!=4 or args[0]!="--duration" or args[2]!="--limit":
-        typeout("----- Nuhuh. To use me, run: github_trending --duration <day> --limit <n>")
-    else:
-        try:
-            day = int(args[1])
-        except ValueError:
-            typeout("----- Nuhuh. To use me, run: github_trending --duration <day> --limit <n>")
-            typeout("----- Hint: use integer for duration time")
-            return
-        try:
-            limit = int(args[3])
-        except ValueError:
-            typeout("----- Nuhuh. To use me, run: github_trending --duration <day> --limit <n>")
-            typeout("----- Hint: use integer for limit number")       
-            return
-        
+        day=7
+        limit=10
         from_date_obj = TODAY_OBJ - timedelta(days=day)
         from_date = from_date_obj.strftime('%Y-%m-%d')
-        print(from_date)
+        fetch_data(from_date, limit)      
+    elif len(args)==1 and args[0]=="greet":
+        show_greetings()
+    elif len(args)==1 and args[0]!="greet":
+        error_messege()
+        return
+    elif len(args)==2:
+        day = 7
+        limit = 10
+        if args[0]=="--duration":
+            day = int(args[1])
+        elif args[0]=="--limit":
+            limit = int(args[1])
+        else:
+            error_messege()
+            return
+        from_date_obj = TODAY_OBJ - timedelta(days=day)
+        from_date = from_date_obj.strftime('%Y-%m-%d')
+        fetch_data(from_date, limit)
+    elif len(args)==4:
+        if args[0]=="--duration" and args[2]=="--limit":
+            day = int(args[1]) 
+            limit = int(args[3])
+        elif args[2]=="--duration" and args[0]=="--limit":
+            day = int(args[3]) 
+            limit = int(args[1])
+        else:
+            error_messege()
+            return
+        from_date_obj = TODAY_OBJ - timedelta(days=day)
+        from_date = from_date_obj.strftime('%Y-%m-%d')
+        fetch_data(from_date, limit)
         
-        
-    
 if __name__ == "__main__":
     main()
